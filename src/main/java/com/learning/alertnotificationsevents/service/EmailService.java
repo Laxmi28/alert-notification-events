2a2e53cce8a878db6daf100477799d59c2b5afa4
laxmi <laxpandey28@gmail.com>
2026-04-28 13:59:28 +0530

    Use of concurrent hashmap to implement caching

--- src/main/java/com/learning/alertnotificationsevents/service/EmailService.java
+++ src/main/java/com/learning/alertnotificationsevents/service/EmailService.java
@@ -13,11 +13,15 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 public class EmailService {
 
     private static final Logger log = LoggerFactory.getLogger(EmailService.class);
+
+    private Map<String, Boolean> map = new ConcurrentHashMap<>();
 
 
     private final KafkaTemplate<String, NotificationEvent> kafkaTemplate;
@@ -32,33 +36,39 @@ public class EmailService {
     @CircuitBreaker(name = "emailService", fallbackMethod = "fallback")
     public void sendMail(NotificationEvent event) {
 
-
-        NotificationEntity entity = new NotificationEntity();
-        entity.setUserId(event.getUserId());
-        entity.setMessage(event.getMessage());
-        entity.setType(event.getType());
-        entity.setRetryCount(event.getRetryCount());
-        entity.setCreatedAt(LocalDateTime.now());
-        entity.setPriority(event.getPriority());
-
-        try {
-            if (event.getMessage().contains("fail")) {
-                throw new RuntimeException("The message consumption failed");
-            }
-            log.info("Email sent for event id : {} ", event.getId());
-            entity.setStatus("SENT");
-        } catch (Exception ex) {
-            log.error("Email failed for event id : {} ", event.getId());
-            entity.setStatus("FAILED");
-            repository.save(entity);
-            throw ex;
-
+        if ( map.containsKey(event.getId())){
+            log.info("Email already sended");
+            return;
         }
 
-        repository.save(entity);
+        else {
 
+            NotificationEntity entity = new NotificationEntity();
+            entity.setUserId(event.getUserId());
+            entity.setMessage(event.getMessage());
+            entity.setType(event.getType());
+            entity.setRetryCount(event.getRetryCount());
+            entity.setCreatedAt(LocalDateTime.now());
+            entity.setPriority(event.getPriority());
 
-        log.info("Email sent successfully");
+            try {
+                if (event.getMessage().contains("fail")) {
+                    throw new RuntimeException("The message consumption failed");
+                }
+                log.info("Email sent for event id : {} ", event.getId());
+                entity.setStatus("SENT");
+            } catch (Exception ex) {
+                log.error("Email failed for event id : {} ", event.getId());
+                entity.setStatus("FAILED");
+                repository.save(entity);
+                throw ex;
+
+            }
+
+            repository.save(entity);
+            map.put(event.getId(),true);
+            log.info("Email sent successfully");
+        }
 
     }
 
